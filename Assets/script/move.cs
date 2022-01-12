634074f90ec67a94db6f5af32fa14a519f4e9b78
Yoonjun Kim <mmoaoon159@naver.com>
2022-01-12 19:27:30 +0900

    움직임 제어

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    public float speed;
-    Rigidbody rig;
+    public ParticleSystem ps;
+
     // Start is called before the first frame update
     void Start()
     {
-        rig = gameObject.GetComponent<Rigidbody>();
+
     }
 
     void FixedUpdate()
     {
-        float time = Time.deltaTime;
-        if (Input.GetKey(KeyCode.W))
-            rig.AddForce(Vector3.up * speed);
-        if (Input.GetKey(KeyCode.S))
-            rig.AddForce(Vector3.down * speed);
-        if (Input.GetKey(KeyCode.A))
-            rig.AddForce(Vector3.left * speed);
-        if (Input.GetKey(KeyCode.D))
-            rig.AddForce(Vector3.right * speed);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            this.transform.Translate(-0.1f, 0.0f, 0.0f);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            this.transform.Translate(0.1f, 0.0f, 0.0f);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            this.transform.Translate(0.0f, 0.1f, 0.0f);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            this.transform.Translate(0.0f, -0.1f, 0.0f);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            ps.Play();
+        if (Input.GetKeyUp(KeyCode.Space))
+            ps.Stop();
     }
 }
